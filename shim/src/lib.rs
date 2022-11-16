9a59bd4233853f8da08ce9d1a823761bb0d58d97
Paul Howard <paul.howard@arm.com>
2022-11-16 21:20:34 +0000

    Wire up to the actual Rust api-client in place of the fake data.
    Signed-off-by: Paul Howard <paul.howard@arm.com>

--- shim/src/lib.rs
+++ shim/src/lib.rs
@@ -5,8 +5,9 @@ use std::ffi::{c_void, CStr, CString};
 
 use core::slice;
 
-use veraison_apiclient::{ChallengeResponseBuilder, Error, Nonce};
+use veraison_apiclient::{ChallengeResponseBuilder, Nonce};
 
+/// C-compatible representation of the challenge-response session.
 #[repr(C)]
 pub struct ShimRawChallengeResponseSession {
     session_url: *const libc::c_char,
@@ -14,9 +15,12 @@ pub struct ShimRawChallengeResponseSession {
     nonce: *const u8,
     accept_type_count: libc::size_t,
     accept_type_list: *const *const libc::c_char,
-    session_wrapper: *mut c_void,
+    session_wrapper: *mut c_void, // Opaque pointer to ShimCallengeResponseSession
 }
 
+/// This structure contains the Rust-managed objects that are behind the raw pointers sent back to C
+/// world. This structure is not visible to C other than as an opaque (void*) pointer. Think of it as
+/// being like the private part of a public/private interface.
 struct ShimChallengeResponseSession {
     session_url_cstring: CString,
     nonce_vec: Vec<u8>,
@@ -31,11 +35,14 @@ pub extern "C" fn open_challenge_response_session(
     nonce: *const u8,
     out_session: *mut *mut ShimRawChallengeResponseSession,
 ) -> u32 {
+    // Unsafe region because we have to trust the caller's char* ptr.
     let url_str: &str = unsafe {
         let url_cstr = CStr::from_ptr(base_url);
         url_cstr.to_str().unwrap()
     };
 
+    // Make a Nonce variant according to the given nonce_size and nonce arguments. If the nonce is null,
+    // this implies the Nonce::Size() variant, otherwise it's the Nonce::Value() variant.
     let nonce_converted: Nonce = {
         if nonce == std::ptr::null() {
             // Null pointer implies a request for the server to generate the nonce
@@ -44,55 +51,62 @@ pub extern "C" fn open_challenge_response_session(
             Nonce::Size(nonce_size)
         } else {
             // Non-null pointer means we are making a Nonce::Value variant of the
-            // given size.
+            // given size. We have to trust the caller's pointer here, hence unsafe region.
             let bytes = unsafe { slice::from_raw_parts(nonce, nonce_size) };
             Nonce::Value(Vec::from(bytes))
         }
     };
 
-    // From here on in, just fake everything to get the FFI layer correct.
-    let fake_nonce = match nonce_converted {
-        // If the nonce is a size, just ignore the size and use a vector of 8 bytes as the fake
-        // server-generated nonce.
-        Nonce::Size(_) => vec![0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08],
+    // Establish the client session.
+    // TODO(paulhowardarm) - Using unwrap() here. We need to map errors to a suitable return status because we're
+    //                       in a C-callable context here.
+    let cr = ChallengeResponseBuilder::new()
+        .with_base_url(String::from(url_str))
+        .build()
+        .unwrap();
 
-        // Nonce is supplied by the caller, so just clone it.
-        Nonce::Value(v) => v.clone(),
-    };
+    let (session_uri, session) = cr.new_session(&nonce_converted).unwrap();
 
-    let mut fake_media_types = Vec::new();
-    fake_media_types.push(String::from("application/fake-test-media-type-1"));
-    fake_media_types.push(String::from("application/fake-test-media-type-2"));
-    fake_media_types.push(String::from("application/fake-test-media-type-3"));
-    fake_media_types.push(String::from("application/fake-test-media-type-4"));
+    let session_nonce = session.nonce().to_vec();
+    let session_accept_types = session.accept().to_vec();
 
-    let mut fake_session_url = String::from(url_str);
-    fake_session_url.push_str("/sessions/fake/1234");
-
-    let media_type_cstrings = fake_media_types
+    // Map the Rust Strings to CString objects for the accept types.
+    let media_type_cstrings = session_accept_types
         .iter()
         .map(|s| CString::new(s.as_str()).unwrap())
         .collect();
+
+    // Make the ShimChallengeResponse session, which houses the Rust-compatible objects in order to manage
+    // their memory in Rust world. This object is not visible to C world other than as an opaque pointer
+    // that can be recovered later.
     let mut shim_session = ShimChallengeResponseSession {
-        session_url_cstring: CString::new(fake_session_url.as_str()).unwrap(),
-        nonce_vec: fake_nonce.clone(),
+        session_url_cstring: CString::new(session_uri.as_str()).unwrap(),
+        nonce_vec: session_nonce.clone(),
         accept_type_cstring_vec: media_type_cstrings,
-        accept_type_ptr_vec: Vec::with_capacity(fake_media_types.len()),
+        accept_type_ptr_vec: Vec::with_capacity(session_accept_types.len()),
     };
 
+    // Get the ptr (char*) for each CString and also store that in a Rust-managed Vec.
     for s in &shim_session.accept_type_cstring_vec {
         shim_session.accept_type_ptr_vec.push(s.as_ptr())
     }
 
+    // Now make the ShimRawChallengeResponseSession, which houses the C-compatible types and is the structure
+    // that C world sees. It is made out of interior pointers into the Rust data structure above.
     let raw_shim_session = Box::new(ShimRawChallengeResponseSession {
         session_url: shim_session.session_url_cstring.as_ptr(),
         nonce_size: shim_session.nonce_vec.len(),
         nonce: shim_session.nonce_vec.as_ptr(),
         accept_type_count: shim_session.accept_type_ptr_vec.len(),
         accept_type_list: shim_session.accept_type_ptr_vec.as_ptr(),
+        // Use Box::into_raw() to "release" the Rust memory so that the pointers all remain valid.
         session_wrapper: Box::into_raw(Box::new(shim_session)) as *mut c_void,
     });
 
+    // Finally, use Box::into_raw() again for the raw session, so that Rust doesn't dispose it when it
+    // drops out of scope.
+    // C world will pass this pointer back to us in free_challenge_response_session(), at which point
+    // we do Box::from_raw() to bring the memory back under Rust management.
     let session_ptr = Box::into_raw(raw_shim_session);
     unsafe { *out_session = session_ptr };
 
@@ -105,5 +119,6 @@ pub extern "C" fn free_challenge_response_session(
 ) -> () {
     // Just re-box the session and let Rust drop it all automatically.
     let raw_session = unsafe { Box::from_raw(session) };
-    let _ = unsafe { Box::from_raw(raw_session.session_wrapper as *mut ShimChallengeResponseSession)};
+    let _ =
+        unsafe { Box::from_raw(raw_session.session_wrapper as *mut ShimChallengeResponseSession) };
 }
